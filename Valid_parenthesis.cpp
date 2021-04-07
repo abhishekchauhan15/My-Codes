506fe160066e5bf57333ff2734677b7c14ab8c9c
abhishekchauhan15 <76480451+abhishekchauhan15@users.noreply.github.com>
2021-04-07 23:51:30 +0530

    Add files via upload

---
+++ Valid_parenthesis.cpp
@@ -0,0 +1,23 @@
+#include<iostream> 
+using namespace std;
+string valid_parenthesis(string  exprr){
+    char s; // s is stack 
+    char x;
+
+    for(int i=0; i<exprr.length(); i++){   //  why till length as length is 6 in my case {[()]} so it sholud be till i=5 na
+        if exprr[i]
+    }
+
+
+}
+int main(){
+string exprr ;
+cout<<" enter the  parenthesis ";
+cin>>exprr;
+
+valid_parenthesis(exprr);
+
+
+
+return 0;
+}
